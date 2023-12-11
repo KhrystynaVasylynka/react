8e89afd805bd33af240093c9024263d6c2ff8113
Khrystyna <gavruljak.gmail.com>
2023-12-11 20:38:43 +0200

    created a component

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// without jsx
-// const el = React.createElement("h1",{id: 'title'}, 'Hello title')
 
-// with jsx
-const a = 10
-const el = <h1 id="title">Hello Title, {a}</h1>
-root.render(<React.StrictMode>{el}</React.StrictMode>)
+//компонент
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, ex repellat impedit consectetur molestias omnis ducimus fugiat facere, mollitia officiis repellendus eum, quibusdam debitis quae illum. Odit repudiandae in id!</p>
+    </div>
+  )
+}
+root.render(
+  <React.StrictMode>
+  <App />
+</React.StrictMode>)
