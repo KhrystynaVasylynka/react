fbe2d8a49de3c3bb37ec4df2e1358b5bb6ffbb8e
Khrystyna <gavruljak.gmail.com>
2023-12-12 14:20:49 +0200

    created two components

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,41 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 //компонент
 
-const App = () => {
+const Header = () => {
+  return <h1>Hello world</h1>
+}
+
+const Content = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis, ex repellat impedit consectetur molestias omnis ducimus fugiat facere, mollitia officiis repellendus eum, quibusdam debitis quae illum. Odit repudiandae in id!</p>
-    </div>
+      <div>
+          <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Quaerat quibusdam tempore recusandae autem fuga praesentium
+              consequuntur, voluptas assumenda perferendis, rerum
+              reprehenderit consequatur, molestias iure. Commodi fugit quasi
+              accusamus aperiam corporis.
+          </p>
+          <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est
+              provident aliquam labore sed fugiat dolore cum placeat?
+              Blanditiis, mollitia perferendis eveniet, id veritatis ad rem,
+              voluptatem reiciendis voluptas voluptate sequi.
+          </p>
+      </div>
   )
 }
+
+const App = () => {
+  return (
+      <div>
+          <Header />
+          <Content />
+      </div>
+  )
+}
+
+
 root.render(
   <React.StrictMode>
-  <App />
+    <App />
 </React.StrictMode>)
