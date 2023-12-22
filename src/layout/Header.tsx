9f827d5c4420422e17ef16bc4d4e6360a1ec9ee5
Khrystyna <gavruljak.gmail.com>
2023-12-22 22:16:02 +0200

    added comment

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,13 +1,12 @@
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
+// import Typography from '@mui/material/Typography'
+// import IconButton from '@mui/material/IconButton'
+// import MenuIcon from '@mui/icons-material/Menu'
 import Container from '@mui/material/Container'
 import Logo from 'components/Logo/Logo'
 import Menu from '../components/Menu/Menu'
-import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {}
 
