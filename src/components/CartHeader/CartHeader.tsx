cc57e3fbc3ab8fbcfd5995004df3fe803da5342e
Khrystyna <gavruljak.gmail.com>
2024-01-04 19:53:55 +0200

    added total price

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,27 +1,41 @@
-import {Product, getProductsObject, productsArray } from 'utils/productsArray'
+import { Product, getProductsObject, productsArray } from 'utils/productsArray'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
-    producsObject?: {
-        [id:number]:Product
+    productsObject?: {
+        [id: number]: Product
     }
 }
 
-const CartHeader = ({ productsInCart,
-producsObject = getProductsObject(productsArray),
+const CartHeader = ({
+    productsInCart,
+    productsObject = getProductsObject(productsArray),
 }: Props) => {
     return (
         <div>
-            {Object.keys(productsInCart).map((productId) => {
-                return (
-                    <div key={productId}>
-                        {productsArray[+productId].title}:
-                        {productsInCart[+productId]}
-                    </div>
-                )
-            })}
+            <div>
+                {Object.keys(productsInCart).map((productId) => {
+                    return (
+                        <div key={productId}>
+                            {productsArray[+productId].title}:
+                            {productsInCart[+productId]}
+                        </div>
+                    )
+                })}
+            </div>
+            <div>
+                Total:{' '}
+                {Object.keys(productsInCart).reduce(
+                    (sum, productId) =>
+                        sum +
+                        productsObject[+productId].price *
+                            productsInCart[+productId],
+                    0
+                )}
+                $
+            </div>
         </div>
     )
 }
