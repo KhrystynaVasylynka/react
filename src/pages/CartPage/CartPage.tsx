9d70e27f8c107a09382d4973d4f17d9f6ade094c
Khrystyna <gavruljak.gmail.com>
2024-01-09 14:09:06 +0200

    added CartProductListExtended

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,23 +1,26 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import CartProductList from 'components/CartProductList/CartProductList'
+import CartProductListItemExtended from 'components/CartProductList/CartProductListItemExtended'
 import CartTotal from 'components/CartTotal/CartTotal'
 
 type Props = {
     productsInCart: {
         [id: number]: number
-    }  
+    }
 }
 
-const CartPage = ({
-    productsInCart
-}: Props) => {
+const CartPage = ({ productsInCart }: Props) => {
     return (
         <div>
             <Typography component={'h1'} variant="h4">
                 Cart
             </Typography>
-           <CartProductList productsInCart={productsInCart}/>
-           <CartTotal productsInCart={productsInCart}/>
+            <Grid container spacing={4}>
+            <CartProductList productsInCart={productsInCart}
+            CartItem={CartProductListItemExtended}
+                />
+            </Grid>
+            <CartTotal productsInCart={productsInCart} />
         </div>
     )
 }
