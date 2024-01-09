e6a24c40d57019e6f235592e47117b33ac7772cb
Khrystyna <gavruljak.gmail.com>
2024-01-09 14:55:37 +0200

    added function removeProductsFromCart with omit from lodash

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -4,11 +4,10 @@ import { StyledEngineProvider } from '@mui/material/styles'
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import CartPage from 'pages/CartPage/CartPage'
-import { Container } from "@mui/material"
-import Home from "pages/Home/Home"
+import { Container } from '@mui/material'
+import Home from 'pages/Home/Home'
 import Footer from 'layout/Footer'
-import {omit} from 'lodash'
-
+import { omit } from 'lodash'
 
 type Props = {}
 
@@ -22,39 +21,47 @@ const App = (props: Props) => {
         2: 5,
     })
 
-
-   
     const addProductInCart = (id: number, count: number) => {
         setProductsInCart((prevState) => ({
             ...prevState,
             [id]: (prevState[id] || 0) + count,
         }))
     }
-    const removeProductsFromCart = (id: number) => { 
-        setProductsInCart((prevState) => omit(prevState,id))
+    const removeProductsFromCart = (id: number) => {
+        setProductsInCart((prevState) => omit(prevState, id))
     }
     return (
         <>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
                 <Header productsInCart={productsInCart} />
-                <Container maxWidth="lg" sx={{
-padding:'50px 0',
-		}}
-		>
-                <Routes>
+                <Container
+                    maxWidth="lg"
+                    sx={{
+                        padding: '50px 0',
+                    }}
+                >
+                    <Routes>
                         <Route
-                            path='/'
-                            element={<Home addProductInCart={addProductInCart} />}
+                            path="/"
+                            element={
+                                <Home addProductInCart={addProductInCart} />
+                            }
                         />
                         <Route
-                            path='cart' element={<CartPage
-                                removeProductFromCart={removeProductsFromCart}
-                                productsInCart={productsInCart} />} />
+                            path="cart"
+                            element={
+                                <CartPage
+                                    removeProductFromCart={
+                                        removeProductsFromCart
+                                    }
+                                    productsInCart={productsInCart}
+                                />
+                            }
+                        />
                     </Routes>
                     <Footer />
-                    </Container>
-               
+                </Container>
             </StyledEngineProvider>
         </>
     )
